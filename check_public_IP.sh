85b9d7fd0d6dc07c4cf2cd3f3f3d47a2f1c16927
joaoalencastro <joao.alencastro@gmail.com>
2020-04-12 09:53:38 -0300

    Feat: Adding log rotation

--- check_public_IP.sh
+++ check_public_IP.sh
@@ -2,9 +2,19 @@
 
 # Program to check my public IP address information
 
-TIMESTAMP="$(date +"%x %r %Z")"
+TIMESTAMP="$(date +"%x %R %Z")"
+TIMESTAMP2="$(date +%s)"
 IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 LOG_FILE=/var/log/public_ip.log
+MAX_LOG_FILE_SIZE=2048
+
+# Get file size in bytes
+LOG_FILE_SIZE=`du -b $LOG_FILE | tr -s '\t' ' ' | cut -d' ' -f1`
+
+# Compare to rotate
+if [ $LOG_FILE_SIZE -gt $MAX_LOG_FILE_SIZE ]; then
+	mv $LOG_FILE $LOG_FILE.$TIMESTAMP
+fi
 
 if [ -f "$LOG_FILE" ]; then
 	# The file is there
