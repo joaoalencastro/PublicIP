b0d800b3665d3b331ab0ca1d309bc1cf494534e9
joaoalencastro <joao.alencastro@gmail.com>
2020-04-12 10:36:00 -0300

    Fix: DNS Upstream Server

--- check_public_IP.sh
+++ check_public_IP.sh
@@ -2,9 +2,9 @@
 
 # Program to check my public IP address information
 
-TIMESTAMP="$(date +"%x %R %Z")"
+TIMESTAMP="$(date +"%x %r")"
 TIMESTAMP2="$(date +%s)"
-IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+IP="$(dig +short myip.opendns.com @208.67.222.222)"
 LOG_FILE=/var/log/public_ip.log
 MAX_LOG_FILE_SIZE=2048
 
@@ -24,4 +24,4 @@ if [ $LOG_FILE_SIZE -gt $MAX_LOG_FILE_SIZE ]; then
 	mv $LOG_FILE $LOG_FILE.$TIMESTAMP
 fi
 
-echo "$TIMESTAMP - My public IP was $IP" >> /var/log/public_ip.log
+echo "$TIMESTAMP - My public IP was $IP" >> $LOG_FILE
